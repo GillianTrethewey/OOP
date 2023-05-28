e5374f29cf682409349daf12051dd96f04b9e16b
Gillian Trethewey <gilliantrethewey100@gmail.com>
2023-05-27 22:01:20 -0400

    person class

---
+++ 10 Person Class/personClass.js
@@ -0,0 +1,12 @@
+class PersonClass {
+  constructor(name) {
+    this.name = name;
+  }
+  greet() {
+    console.log("hello");
+  }
+}
+
+const george = new PersonClass("George");
+
+george.greet(); // -> Logs 'hello'
